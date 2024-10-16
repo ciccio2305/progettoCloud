1ad832b59599ea0006591481f346b4595e07ecab
ciccio2305 <56836127+ciccio2305@users.noreply.github.com>
2024-10-16 17:56:38 +0200

    test lancio 1

--- sutupDocker.sh
+++ sutupDocker.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
 # Aggiornamento del sistema
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
 echo "Sistema aggiornato."
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Installazione dei pacchetti necessari
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Aggiunta della chiave GPG di Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-echo "Chiave GPG di Docker aggiunta."
-
-# Aggiunta del repository Docker
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo apt-get update
 
 # Installazione di Docker
-sudo apt-get update -y
-sudo apt-get install -y docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Docker installato."
 
