d33ecd644c4fd4febbb9fd09bea464d36a32043b
ciccio2305 <56836127+ciccio2305@users.noreply.github.com>
2024-10-17 14:12:35 +0200

    test push

--- sutupDocker.sh
+++ sutupDocker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #crea un file con scritto ciao
-echo "Ciao" > ciao.txt
-# Aggiornamento del sistema
-echo "Sistema aggiornato."
+echo "Sistema aggiornato.1" > ciao.txt
+
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install ca-certificates curl
+echo "Sistema aggiornato." > ciao.txt
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "Sistema aggiornato 2 " > ciao.txt
 
 # Add the repository to Apt sources:
 echo \
@@ -22,14 +23,14 @@ sudo apt-get update
 # Installazione di Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "Docker installato."
+echo "Docker installato." > ciao.txt
 
 # Abilitare Docker per l'avvio automatico
 sudo systemctl enable docker
 sudo systemctl start docker
 
 
-echo "Docker abilitato per l'avvio automatico."
+echo "Docker abilitato per l'avvio automatico." > ciao.txt
 
 
 # Aggiunta dell'utente al gruppo docker (opzionale, evita di usare sudo con Docker)
@@ -39,7 +40,7 @@ sudo usermod -aG docker $USER
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
-echo "Minikube installato."
+echo "Minikube installato."> ciao.txt
 
 # Verifica delle installazioni
 docker --version
